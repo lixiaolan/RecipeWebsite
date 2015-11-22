0bda827aa975c7af4e6dfdb2f29593e7b9da3379
qile <jefferis.l@gmail.com>
2015-11-22 13:29:26 -0600

    Commit before making major changes to server

--- model.js
+++ model.js
@@ -1,12 +1,29 @@
-var cookBook = function(recipesObj)
+var cookBook = function(doneLoadingDelegate)
 {
     // Return object
     var that = {};
     
     // Private:
-    var recipes = recipesObj;
-    
-    var newId = function()
+    var recipes = {};
+
+    // Load the data from the server.
+    var loadRecipes = $.ajax("")
+        .done(function ()
+              {
+                  $.parseJSON(loadRecipes.responseText);
+                  doneLoadingDelegate();
+                  return;
+              }
+             );    
+
+    // Public:
+    that.getRecipes = function()
+    {
+        return recipes;
+    };
+
+    // Returns an unused recipe id
+    that.newId = function()
     {
         // Get the smallest id not yet used
         var count = 0;
@@ -20,26 +37,28 @@ var cookBook = function(recipesObj)
         return count.toString();
     };
 
-    // Public:
-    that.getRecipes = function()
+    // Updates or creates recipe with given id.
+    that.modifyRecipe = function(id, title, tags, text)
     {
-        return recipes;
+        // Create empty recipe if it does not exist:
+        recipes[id] = recipes[id] || {};
+
+        // Add the title and tags objects:
+        recipes[id].title = title;
+        recipes[id].tags = tags;
+
+        // TODO: Save the new text based on the title
+
+        return true;
+    };
+
+    that.deleteRecipe = function(id)
+    {
+        delete recipes[id];
+
+        // TODO: Do work to update the database
     };
     
-    that.addRecipe = function(title, tags, text)
-    {
-        var Id = newId();
-        var recipe = {};
-
-        recipe.title = title;
-        recipe.tags = tags;
-        recipes[Id] = recipe;
-
-        // TODO: Save new recipe obj and text to DB
-        
-        return newId;
-    };
-        
     that.getTags = function()
     {
         // Return a set of all tags
@@ -50,7 +69,7 @@ var cookBook = function(recipesObj)
 
             for (var tag in tags)
             {
-                set[tag] = null;
+                set[tag] = 1;
             }
         }
         
@@ -63,30 +82,55 @@ var cookBook = function(recipesObj)
         return array.sort();
     };
 
+    that.getRecipes = function(inTags)
+    {
+        outRecipes = {};
+        for (var recipeId in recipes)
+        {
+            for (var tagKey in recipes[recipeId].tags)
+            {
+                if (inTags[tagKey])
+                    outRecipes[recipeId] = recipes[recipeId];
+            }
+        }
+        return outRecipes;
+    };
+    
     return that;
 }
 
-var recipes = {
-    6 : {
-        "title" : "<title string>",
-        "tags" :
-        {
-            "a" : null,
-            "r" : null,
-            "d" : null,
+var recipes =
+    {
+        6 : {
+            "title" : "<title string>",
+            "tags" :
+            {
+                "a" : 1,
+                "r" : 1,
+                "d" : 1,
+            },
         },
-    },
-}
+        5 : {
+            "title" : "yabababab",
+            "tags" :
+            {
+                "c" : 1,
+                "d" : 1,
+            }
+        },
+    };
+
 
 var test = cookBook(recipes);
 
 var title = "Recipe One";
-var tags = {"d" : null, "e" : null};
+var tags = {"d" : 1, "e" : 1};
 var text = "this is the full text of the recipe";
 
-test.addRecipe(title, tags, text);
+test.modifyRecipe(test.newId(),title, tags, text);
 
 console.log(test.getTags());
+console.log(test.getRecipes({"a" : 1, "d" : 1}));
 
 
 // Not sure if I need this:
